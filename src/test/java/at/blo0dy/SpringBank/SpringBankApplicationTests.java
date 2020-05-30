9476d059e015c7a7cf386bbeba24b1bbfc5e470c
blo0dskAr <blo0dy@chello.at>
2020-05-30 11:39:10 +0200

    initial commit

---
+++ src/test/java/at/blo0dy/SpringBank/SpringBankApplicationTests.java
@@ -0,0 +1,13 @@
+package at.blo0dy.SpringBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

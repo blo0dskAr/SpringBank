80a8643263b7e439c857733a0a621e7254440398
blo0dskAr <blo0dy@chello.at>
2020-07-10 18:59:59 +0200

    minor changes in bankServiceImpl Test

--- src/test/java/at/blo0dy/SpringBank/service/bank/BankServiceImplTest.java
+++ src/test/java/at/blo0dy/SpringBank/service/bank/BankServiceImplTest.java
@@ -7,34 +7,35 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class BankServiceImplTest {
 
   @InjectMocks
+//  @Autowired
   BankServiceImpl bankService;
 
   @Mock
+//  @MockBean
   BankRepository bankRepository;
 
   @Test
   void saveBank() {
-    when(bankRepository.save(new Bank("blo0dy Inc.", "blo0dy", 12345666L))).thenReturn(new Bank("blo0dy Inc.", "blo0dy", 12345666L));
 
-    Bank bank = new Bank("blo0dy Inc.", "blo0dy", 12345666L);
-    bankService.saveBank(bank);
+    bankService.saveBank(new Bank("blo0dy Inc.", "blo0dy", 12345666L));
 
-    assertEquals("blo0dy Inc.", bank.getFirmenName());
-    assertEquals("blo0dy", bank.getFirmenChef());
-    assertEquals(12345666L, bank.getSteuerNummer());
-    assertEquals(1L, bank.getId());
-
+    verify(bankRepository, times(1)).save(any(Bank.class));
   }
 
   @Test

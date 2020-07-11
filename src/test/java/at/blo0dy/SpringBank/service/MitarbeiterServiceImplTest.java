f97253d010b5233e38263d33c8c0e44bb51e5b30
blo0dskAr <blo0dy@chello.at>
2020-07-11 18:39:50 +0200

    minor changes with some testcases (checks on hasNoErrors

---
+++ src/test/java/at/blo0dy/SpringBank/service/MitarbeiterServiceImplTest.java
@@ -0,0 +1,37 @@
+package at.blo0dy.SpringBank.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MitarbeiterServiceImplTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void findAll() {
+  }
+
+  @Test
+  void findById() {
+  }
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void deleteById() {
+  }
+
+  @Test
+  void findMitarbeiterByVorAndNachName() {
+  }
+
+  @Test
+  void count() {
+  }
+}

961d7a875d34df84838a9014f1f7d28d0c2b25f9
blo0dskAr <blo0dy@chello.at>
2020-07-16 12:20:13 +0200

    added kontodetailpage for kredit and giro

--- src/main/java/at/blo0dy/SpringBank/controller/mitarbeiter/crm/giro/CrmGiroKontoController.java
+++ src/main/java/at/blo0dy/SpringBank/controller/mitarbeiter/crm/giro/CrmGiroKontoController.java
@@ -1,24 +1,34 @@
 package at.blo0dy.SpringBank.controller.mitarbeiter.crm.giro;
 
+import at.blo0dy.SpringBank.model.konto.Konto;
+import at.blo0dy.SpringBank.model.person.kunde.Kunde;
 import at.blo0dy.SpringBank.service.konto.giro.GiroService;
-import at.blo0dy.SpringBank.service.konto.sparen.SparService;
+import at.blo0dy.SpringBank.service.konto.zahlungsAuftrag.ZahlungsAuftragService;
 import at.blo0dy.SpringBank.service.kunde.KundeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequestMapping("mitarbeiter/kunde/giro")
 public class CrmGiroKontoController {
 
   GiroService giroService;
   KundeService kundeService;
+  ZahlungsAuftragService zahlungsAuftragService;
 
   @Autowired
-  public CrmGiroKontoController(KundeService kundeService, GiroService giroService) {
+  public CrmGiroKontoController(KundeService kundeService, GiroService giroService, ZahlungsAuftragService zahlungsAuftragService) {
     this.kundeService = kundeService;
     this.giroService = giroService;
+    this.zahlungsAuftragService = zahlungsAuftragService;
   }
 
 
@@ -26,6 +36,24 @@ public class CrmGiroKontoController {
   public String showGiroKontoPage() {
 
     return "mitarbeiter/crm/giro/giroKonto";
+  }
+
+
+  @GetMapping("/konto/showGiroKontoDetailPage")
+  public String showGiroKontoDetailPage(@CurrentSecurityContext(expression = "authentication") Authentication authentication, Model model,
+                                        @RequestParam("giroKontoId") Long giroKontoId) {
+
+    log.debug("Showing GiroKontoDetailPage for Mitarbeiter: " + authentication.getName() + " und KontoId: " + giroKontoId);
+
+    Konto girokonto = giroService.findById(giroKontoId);
+    Kunde kunde = girokonto.getKunde();
+
+    model.addAttribute("girokonto", girokonto);
+    model.addAttribute("kunde", kunde);
+    model.addAttribute("countOffeneZA",zahlungsAuftragService.countOffeneZahlungsAuftraegeByKontoId(giroKontoId));
+
+    return "mitarbeiter/crm/giro/konto-detail";
+
   }
 
 
--- src/main/java/at/blo0dy/SpringBank/controller/mitarbeiter/crm/kredit/CrmKreditKontoController.java
+++ src/main/java/at/blo0dy/SpringBank/controller/mitarbeiter/crm/kredit/CrmKreditKontoController.java
@@ -1,24 +1,34 @@
 package at.blo0dy.SpringBank.controller.mitarbeiter.crm.kredit;
 
+import at.blo0dy.SpringBank.model.konto.Konto;
+import at.blo0dy.SpringBank.model.person.kunde.Kunde;
 import at.blo0dy.SpringBank.service.konto.kredit.KreditService;
-import at.blo0dy.SpringBank.service.konto.sparen.SparService;
+import at.blo0dy.SpringBank.service.konto.zahlungsAuftrag.ZahlungsAuftragService;
 import at.blo0dy.SpringBank.service.kunde.KundeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequestMapping("mitarbeiter/kunde/kredit")
 public class CrmKreditKontoController {
 
   KreditService kreditService;
   KundeService kundeService;
+  ZahlungsAuftragService zahlungsAuftragService;
 
   @Autowired
-  public CrmKreditKontoController(KundeService kundeService, KreditService kreditService) {
+  public CrmKreditKontoController(KundeService kundeService, KreditService kreditService, ZahlungsAuftragService zahlungsAuftragService) {
     this.kundeService = kundeService;
     this.kreditService = kreditService;
+    this.zahlungsAuftragService = zahlungsAuftragService;
   }
 
 
@@ -26,6 +36,24 @@ public class CrmKreditKontoController {
   public String showKreditKontoPage() {
 
     return "mitarbeiter/crm/kredit/kreditKonto";
+  }
+
+
+  @GetMapping("/konto/showKreditKontoDetailPage")
+  public String showKreditKontoDetailPage(@CurrentSecurityContext(expression = "authentication") Authentication authentication, Model model,
+                                        @RequestParam("kreditKontoId") Long kreditKontoId) {
+
+    log.debug("Showing KreditKontoDetailPage for Mitarbeiter: " + authentication.getName() + " und KontoId: " + kreditKontoId);
+
+    Konto kreditkonto = kreditService.findById(kreditKontoId);
+    Kunde kunde = kreditkonto.getKunde();
+
+    model.addAttribute("kreditkonto", kreditkonto);
+    model.addAttribute("kunde", kunde);
+    model.addAttribute("countOffeneZA",zahlungsAuftragService.countOffeneZahlungsAuftraegeByKontoId(kreditKontoId));
+
+    return "mitarbeiter/crm/kredit/konto-detail";
+
   }
 
 

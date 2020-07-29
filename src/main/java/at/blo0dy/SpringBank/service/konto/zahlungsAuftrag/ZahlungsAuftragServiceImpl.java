c55ef44463911325f10765b08512c0b6e2b54cba
blo0dskAr <blo0dy@chello.at>
2020-07-29 16:50:12 +0200

    adding logging to zahlungsauftragservice

--- src/main/java/at/blo0dy/SpringBank/service/konto/zahlungsAuftrag/ZahlungsAuftragServiceImpl.java
+++ src/main/java/at/blo0dy/SpringBank/service/konto/zahlungsAuftrag/ZahlungsAuftragServiceImpl.java
@@ -12,6 +12,7 @@ import at.blo0dy.SpringBank.model.konto.giro.GiroKonto;
 import at.blo0dy.SpringBank.model.konto.kontoBuchung.KontoBuchung;
 import at.blo0dy.SpringBank.model.konto.zahlungsAuftrag.ZahlungsAuftrag;
 import at.blo0dy.SpringBank.model.zv.Datentraeger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +22,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ZahlungsAuftragServiceImpl implements ZahlungsAuftragService{
 
@@ -40,36 +42,44 @@ public class ZahlungsAuftragServiceImpl implements ZahlungsAuftragService{
   @Override
 //  @Transactional
   public ZahlungsAuftrag save(ZahlungsAuftrag zahlungsAuftrag) {
-    return zahlungsAuftragRepository.save(zahlungsAuftrag);
+    log.debug("Zahlungsauftrag wird gespeichert.");
+
+    ZahlungsAuftrag savedZahlungsAuftrag = zahlungsAuftragRepository.save(zahlungsAuftrag);
+    log.debug("Zahlungsauftrag wurde erfolgreich gespeichert. ID: " + savedZahlungsAuftrag.getId());
+
+    return savedZahlungsAuftrag;
   }
 
   @Override
   @Transactional(readOnly = true)
   public List<ZahlungsAuftrag> findZahlungsAuftraegeByKontonummer(String kontonummer) {
+    log.debug("Zahlungsaufträge für Kontonummer: " + kontonummer + " werden gesucht.");
     return zahlungsAuftragRepository.findZahlungsAuftraegeByKontonummer(kontonummer);
   }
 
   @Override
   @Transactional(readOnly = true)
   public Long countOffeneZahlungsAuftraegeByKontoId(Long kontoId) {
+    log.debug("Anzahl offener Zahlungsaufträge for KontoId: " + " wird ermittelt");
     return zahlungsAuftragRepository.countOffeneZahlungsAuftraegeByKontoId(kontoId);
   }
 
-//  @Override
-//  @Transactional(readOnly = true)
-//  public BigDecimal getSummeOffenerAuszahlungenByKontoId(Long kontoId) {
-//    return zahlungsAuftragRepository.getSummeOffenerAuszahlungsAuftraegeByKontoId(kontoId);
-//  }
+/*  @Override
+  @Transactional(readOnly = true)
+  public BigDecimal getSummeOffenerAuszahlungenByKontoId(Long kontoId) {
+    return zahlungsAuftragRepository.getSummeOffenerAuszahlungsAuftraegeByKontoId(kontoId);
+  }*/
 
   @Override
   @Transactional(readOnly = true)
   public boolean checkAuszahlungWithVerfuegbarerBetrag(Konto konto, BigDecimal auszahlungsBetrag) {
-
+    log.debug("Auszahlung von EUR " +  auszahlungsBetrag + " wird mit verfügbaren Betrg am Konto: " + konto.getKontonummer() + " geprüft.");
     BigDecimal verfuegbarerBetrag = getVerfügbarerSaldoByKontoId(konto.getId());
-
     if (verfuegbarerBetrag.compareTo(auszahlungsBetrag) == -1) {
+      log.debug("--> Betrag nicht verfügbar.");
       return false;
     } else {
+      log.debug("--> Betrag verfügbar.");
       return true;
     }
   }
@@ -77,7 +87,7 @@ public class ZahlungsAuftragServiceImpl implements ZahlungsAuftragService{
   @Override
   @Transactional(readOnly = true)
   public List<ZahlungsAuftrag> findAllAngelegteZahlungsAuftraegeByDateAndType(LocalDate auftragsdatum, String type) {
-
+    log.debug("Angelegte Zahlungsaufträge mit Datum: " + auftragsdatum + "und Typ: " + type + " werden gesucht.");
     return zahlungsAuftragRepository.findAllAngelegteZahlungsAuftraegeByDateAndType(auftragsdatum, type);
   }
 
@@ -85,6 +95,7 @@ public class ZahlungsAuftragServiceImpl implements ZahlungsAuftragService{
   @Transactional
   public BigDecimal processSingleZahlungsAuftrag(ZahlungsAuftrag zahlungsAuftrag, Datentraeger datentraeger) {
 
+    log.debug("Zahlungsauftrag wird bearbeitet.");
     // Hier das Konto aus der DB neu laden, weil sonst aus dem zahlungsauftrag.getkonto() der alte saldo übermittelt wird bei > 1 buchung.
     Konto tmpKonto = kontoRepository.findByKontonummer(Long.valueOf(zahlungsAuftrag.getKontonummer()));
     BigDecimal neuerSaldo;
@@ -112,38 +123,48 @@ public class ZahlungsAuftragServiceImpl implements ZahlungsAuftragService{
       buchungsArt = BuchungsArtEnum.HABEN;
     }
 
+    log.debug("Prüfung erfolgreich " + buchungsText + " wird erstellt");
+
     neueKontoBuchung =  new KontoBuchung(0L, LocalDate.now(),LocalDateTime.now(),null,zahlungsAuftrag.getBetrag(), neuerSaldo,buchungsText,tmpKonto, buchungsArt );
 
     // Update KontoSaldo
     // TODO: jetzt hab ichs erst checkt was das Transactional genau macht. Da ein update machen ist bescheuert, durchs Transactional wird der neue Saldo in der DB schon nur beim  setten persistiert....
 //    kontoRepository.updateKontoSaldoById(tmpKonto.getId(),neuerSaldo);
+    log.debug("Saldo für Konto: " + tmpKonto.getKontonummer() + " wird aktualisiert --> Alt: " + tmpKonto.getAktSaldo() + " - Neu: " + neuerSaldo);
     tmpKonto.setAktSaldo(neuerSaldo);
     // Speichere Buchung
-    kontoBuchungRepository.save(neueKontoBuchung);
+    KontoBuchung savedKontoBuchung = kontoBuchungRepository.save(neueKontoBuchung);
+    log.debug("KontoBuchung erfolgreich durchgeführt. ID: " + savedKontoBuchung.getId());
 
     // Update ZahlungsAuftrag
 //    zahlungsAuftragRepository.UpdateZahlungsAuftragById(zahlungsAuftrag.getId(), LocalDateTime.now(), ZahlungAuftragStatusEnum.DURCHGEFUEHRT.toString(), datentraeger);
     zahlungsAuftrag.setDatAend(LocalDateTime.now());
     zahlungsAuftrag.setAuftragsStatus(ZahlungAuftragStatusEnum.DURCHGEFUEHRT);
     zahlungsAuftrag.setDatentraeger(datentraeger);
+    log.debug("Zahlungsauftrag erfolgreich auf Datenträger: " + datentraeger.getId() + " gespeichert.");
     return neuerSaldo;
   }
 
   @Override
   @Transactional
   public String processZahlungsAuftragsBatch(ZahlungsAuftrag zahlungsAuftragsSample) {
+    log.debug("ZahlungsAuftragsBatch für " + zahlungsAuftragsSample.getAuftragsArt() + " wird durchgeführt.");
 
     List<ZahlungsAuftrag> zahlungsAuftragsList = findAllAngelegteZahlungsAuftraegeByDateAndType(zahlungsAuftragsSample.getAuftragsDatum(), zahlungsAuftragsSample.getAuftragsArt().toString());
 
     if (zahlungsAuftragsList.isEmpty()) {
+      log.debug("Keine Daten zum verarbeiten gefunden.");
       return "Keine Daten zum verarbeiten gefunden";
     }
 
+    log.debug("Datenträger wird erstellt.");
     Datentraeger datentraeger = new Datentraeger(0, BigDecimal.ZERO, LocalDateTime.now(), null, zahlungsAuftragsSample.getAuftragsArt());
-    datenTraegerRepository.save(datentraeger);
+    Datentraeger savedDatentraeger = datenTraegerRepository.save(datentraeger);
+    log.debug("Datenträger erfolgreich gespeichert. ID: " + datentraeger.getId());
 
     zahlungsAuftragsList.forEach(za ->  processSingleZahlungsAuftrag(za, datentraeger));
 
+    log.debug("Alle Zahlungsaufträge erfolgreich verarbeitet.");
     int anzahlZahlungsAuftraege = zahlungsAuftragRepository.countByDatentraeger(datentraeger);
     BigDecimal summeZahlungsAuftraege = zahlungsAuftragRepository.sumByDatentraeger(datentraeger);
 
@@ -157,6 +178,7 @@ public class ZahlungsAuftragServiceImpl implements ZahlungsAuftragService{
   @Transactional(readOnly = true)
   public BigDecimal getVerfügbarerSaldoByKontoId(Long kontoId) {
 
+    log.debug("Verfügbarer Saldo für KontoId: " + kontoId + " wird ermittelt.");
     Konto konto = kontoRepository.findById(kontoId).get();
 
     BigDecimal verfügbarerBetrag = konto.getAktSaldo().subtract(zahlungsAuftragRepository.getSummeOffenerAuszahlungsAuftraegeByKontoId(kontoId)) ;

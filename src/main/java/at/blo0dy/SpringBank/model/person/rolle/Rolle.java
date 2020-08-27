1bbda257189e9da08576587327dd96c2f316762c
blo0dskAr <blo0dy@chello.at>
2020-08-27 11:46:36 +0200

    minibugfix: FormError bei RoleForm verbessert

--- src/main/java/at/blo0dy/SpringBank/model/person/rolle/Rolle.java
+++ src/main/java/at/blo0dy/SpringBank/model/person/rolle/Rolle.java
@@ -22,13 +22,9 @@ public class Rolle  {
   private Long id;
 
   @Column
-  @NotBlank
-  @Pattern(regexp = "^[a-z_-]{3,20}")
+  @NotBlank(message = "Darf nicht leer sein. Darf nur Buchstaben, \"_\" und \"-\" enthalten")
+  @Pattern(message = "Darf nicht leer sein. Darf nur Buchstaben, \"_\" und \"-\" enthalten", regexp = "^[a-z_-]{3,20}")
   private String name;
-
-/*  @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.DETACH,CascadeType.REFRESH})
-  @JoinColumn(name="mitarbeiter_id")
-  private Mitarbeiter mitarbeiter;*/
 
   @ManyToMany(mappedBy = "rollen")
   private List<Mitarbeiter> mitarbeiter;
